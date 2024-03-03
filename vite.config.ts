68e61c30073b8e6ca9865118ec98ac5ce799abac
mrLuisFer <mrluisfeer@gmail.com>
2024-03-02 21:05:30 -0600

    remove aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dtsPlugin from 'vite-plugin-dts';
 import rollupNodeResolve from '@rollup/plugin-node-resolve';
-import rollupPeerDepsPlugin from 'rollup-plugin-peer-deps-external';
 import rollupCommonPlugin from '@rollup/plugin-commonjs';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,17 +13,8 @@ export default defineConfig({
 		}),
 		rollupNodeResolve(),
 		rollupCommonPlugin(),
-		// @ts-expect-error
-		rollupPeerDepsPlugin(),
 	],
 	define: {
 		__APP_ENV__: process.env.VITE_VERCEL_ENV,
-	},
-
-	resolve: {
-		alias: {
-			'@lib': path.resolve(__dirname, './src/lib'),
-			'@challenges': path.resolve(__dirname, './src/challenges'),
-		},
 	},
 });
