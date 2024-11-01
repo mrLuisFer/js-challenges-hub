b6d296597e4085ea2e4beb62a5b0ecd13348f4c5
mrLuisFer <mrluisfeer@gmail.com>
2024-10-31 18:03:19 -0600

    fix linter stuff

--- src/challenges/interactive-comments/components/Reply/index.tsx
+++ src/challenges/interactive-comments/components/Reply/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { useGetUser } from '../../hooks/useGetUser';
 import { useCommentActions } from '../../hooks/useCommentActions';
 import {
